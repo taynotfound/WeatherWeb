3e730ff684e28a04169755ff0caa33558e73aa24
taynotfound <marztayron@gmail.com>
2024-07-07 20:43:24 +0200

    chore: Update Astro configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
+import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: node({
+    mode: "standalone"
+  })
 });
